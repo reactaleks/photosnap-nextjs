39f3dd88540c2bff9ae529f9fd7c508ea3454d07
Aleks <a.ivanov.developer@gmail.com>
2024-08-21 09:38:48 +0100

    Reworked. Image story component to display data from array of objects.

--- components/image_story_component/ImageStoryComponent.jsx
+++ components/image_story_component/ImageStoryComponent.jsx
@@ -1,24 +1,66 @@
 import LinkMore from "../shared_ui_components/Link_MoreComponent";
 import Image from "next/image";
 export default function ImageStory() {
-  return (
-    <div className="w-full h-[375px] bg-orange-500 flex flex-col relative items-center">
+
+  const storyData = [
+    {
+      'link': "/assets/stories/desktop/mountains.jpg",
+      'height': 500,
+      'width': 360,
+      'alt': '',
+      'title': 'The Mountains',
+      'author': 'by John Appleseed',
+    },
+    {
+      'link': '/assets/stories/desktop/cityscapes.jpg',
+      'height': 500,
+      'width': 360,
+      'alt': '',
+      'title': 'Sunset Cityscapes',
+      'author': 'by Benjamin Cruz',
+    },
+    {
+      'link': '/assets/stories/desktop/18-days-voyage.jpg',
+      'height': 500,
+      'width': 360,
+      'alt': '',
+      'title': '18 Days Voyage',
+      'author': 'by Alexei Borodin',
+    },
+    {
+      'link': '/assets/stories/desktop/architecturals.jpg',
+      'height': 500,
+      'width': 360,
+      'alt': '',
+      'title': 'Architecturals',
+      'author': 'by Samantha Brooke',
+    },
+  ]
+
+  const componentList = storyData.map((item, index) => { 
+    return <div key={index} className="w-full h-[375px] flex flex-col relative items-center">
       <Image
-        src={"/assets/stories/desktop/mountains.jpg"}
+        src={item.link}
         className="w-full h-[375px] object-cover brightness-50"
-        width={360}
-        height={500}
-        alt=""
+        width={item.width}
+        height={item.height}
+        alt={item.alt}
       />
       <div className="absolute bottom-12 text-white w-[310px] h-[99px] flex flex-col justify-around">
         <div className="flex flex-col">
-          <div className="text-[18px] leading-[25px] font-bold">The Mountains</div>
-          <div className="text-[13px]">by John Appleseed</div>
+          <div className="text-[18px] leading-[25px] font-bold">{item.title}</div>
+          <div className="text-[13px]">{item.author}</div>
         </div>
 
         <div className="w-[310px] h-[1px] bg-white bg-opacity-25"></div>
         <LinkMore linkText={"read story"} />
       </div>
     </div>
+  })
+
+  return (
+    <>
+        {componentList}
+    </>
   );
 }
