b4bdff4f54870c50a850a51e099dffc93190de1f
Aleks <a.ivanov.developer@gmail.com>
2024-08-22 12:04:29 +0100

    footer nav menu. added. links to pages.

--- components/footer_comonent/footer_ui_components.jsx/footer_nav_component.jsx
+++ components/footer_comonent/footer_ui_components.jsx/footer_nav_component.jsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 export default function FooterNav() {
   const navItems = ['home', 'Stories', 'Features', 'Pricing']
 
   const componentLst = navItems.map((item, index) => {
     return (
       <div key={index} className="  w-[310px] h-[20px] md:w-[61px] md:h-[16px] xl:justify-start h flex items-center justify-center hover:opacity-30 cursor-pointer">
-        {item}
+        
+        <Link href={item == 'home' ? '/' : item.toLowerCase()}>{item}</Link>
+        
       </div>
     )
   })
